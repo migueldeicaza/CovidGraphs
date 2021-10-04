59a71914e061de7bd15ca5fe1c635c7952420c5f
Kyle Rubenok <kyle@rubenok.ca>
2021-10-03 20:31:11 -0700

    Change split to ; to accept locations with commas
    Better supports locations such as "British Columbia, Canada"

--- CovidGraphs/CovidGraphsApp.swift
+++ CovidGraphs/CovidGraphsApp.swift
@@ -10,13 +10,13 @@ import SwiftUI
 @main
 struct CovidGraphsApp: App {
     @AppStorage ("locations")
-    var locations: String = "Massachusetts,California,Mexico,25021.0,25025.0,Vermont"
+    var locations: String = "Massachusetts;California;Mexico;25021.0;25025.0;Vermont"
     
     func makeLocations (input: String) -> [UpdatableStat]
     {
         var res: [UpdatableStat] = []
         
-        for x in input.split(separator: ",") {
+        for x in input.split(separator: ";") {
             res.append (UpdatableStat (code: String (x)))
         }
         return res
